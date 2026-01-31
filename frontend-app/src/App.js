aa042c82bcc82bd91f8ab3e95a1af82f554ed318
n-kirankumar <nkiran.appdev@gmail.com>
2026-01-31 23:34:52 +0530

    Initial commit: simple react app with docker and ci

---
+++ frontend-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello DevOps 🚀</h1>
+      <p>Very simple React application</p>
+    </div>
+  );
+}
+
+export default App;
+
